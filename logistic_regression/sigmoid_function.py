3067256227f7fd52a699b4782467f7824dd16492
uavashi <uravashiagrawalofficial@gmail.com>
2026-04-18 19:37:04 +0530

    step function

--- logistic_regression/sigmoid_function.py
+++ logistic_regression/sigmoid_function.py
@@ -7,3 +7,34 @@ import matplotlib.pyplot as plt
 plt.figure(figsize=(10,6))
 plt.scatter(x[:,0],x[:,1], c=y, cmap ='winter', s=100)
 plt.show()
+
+
+# step function
+def perceptron(x,y):
+	x = np.insert(x,0,1,axis =1)
+	weights = np.ones(x.shape[1])
+	lr = 0.1
+
+	for i in range(1000):
+		j = np.random.randint(0,100)
+		y_hat = step(np.dot(x[j], weights))
+		weights = weights + lr*(y[j]-y_hat)*x[j]
+
+	return weights[0], weights[1:]
+
+def step(z):
+	return 1 if z>0 else 0
+
+intercept_,coef_ = perceptron(x,y)
+print(coef_)
+m = -(coef_[0]/coef_[1])
+b = -(intercept_/coef_[1])
+x_input = np.linspace(-3,3,100)
+y_input = m*x_input + b
+plt.figure(figsize=(10,6))
+plt.plot(x_input,y_input,color='red',linewidth=3)
+plt.scatter(x[:,0],x[:,1],c=y,cmap='winter',s=100)
+plt.ylim(-3,2)
+plt.show()
+
+
