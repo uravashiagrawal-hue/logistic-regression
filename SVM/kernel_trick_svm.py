245ebe7084f4e01503326fbd1df7d0edcbd1661d
uavashi <uravashiagrawalofficial@gmail.com>
2026-06-26 17:02:37 +0530

    exp and polynomial kernel

--- SVM/kernel_trick_svm.py
+++ SVM/kernel_trick_svm.py
@@ -9,5 +9,68 @@ from matplotlib.colors import ListedColormap
 
 from sklearn.datasets._samples_generator import make_circles
 X,y = make_circles(100, factor=.1, noise=.1)
-plt.scatter(X[:,0], X[:,1], c=y, s=50, cmap='bwr')
+plt.scatter(X[:,0], X[:,1], c=y, s=40, cmap='bwr')
+plt.show()
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+classifier = SVC(kernel="linear")
+classifier.fit(X_train,y_train.ravel())
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, y_pred))
+
+zero_one_colourmap = ListedColormap(('blue', 'red'))
+def plot_decision_boundary(X, y, clf):
+    X_set, y_set = X, y
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1,
+                                 stop = X_set[:, 0].max() + 1,
+                                 step = 0.01),
+                       np.arange(start = X_set[:, 1].min() - 1,
+                                 stop = X_set[:, 1].max() + 1,
+                                 step = 0.01))
+
+    plt.contourf(X1, X2, clf.predict(np.array([X1.ravel(),
+                                             X2.ravel()]).T).reshape(X1.shape),
+               alpha = 0.75,
+               cmap = zero_one_colourmap)
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = (zero_one_colourmap)(i), label = j)
+    plt.title('SVM Decision Boundary')
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.legend()
+    return plt.show()
+plot_decision_boundary(X, y, classifier)
+plt.show()
+
+
+def plot_3d_plot(X, y):
+    r = np.exp(-(X ** 2).sum(1))
+    ax = plt.subplot(projection='3d')
+    ax.scatter3D(X[:, 0], X[:, 1], r, c=y, s=100, cmap='bwr')
+    ax.set_xlabel('X1')
+    ax.set_ylabel('X2')
+    ax.set_zlabel('y')
+    return ax
+plot_3d_plot(X,y)
+plt.show()
+
+rbf_classifier = SVC(kernel="rbf")
+rbf_classifier.fit(X_train, y_train)
+y_pred = rbf_classifier.predict(X_test)
+print(accuracy_score(y_test, y_pred))
+plot_decision_boundary(X, y, rbf_classifier)
+plt.show()
+
+
+poly_classifier = SVC(kernel="poly",degree=2)
+poly_classifier.fit(X_train, y_train)
+y_pred = poly_classifier.predict(X_test)
+print(accuracy_score(y_test, y_pred))
+plot_decision_boundary(X, y, poly_classifier)
 plt.show()
