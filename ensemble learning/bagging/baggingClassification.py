bdd59b360f62f027a4862067c0d0d56a2bf7a4b2
uavashi <uravashiagrawalofficial@gmail.com>
2026-06-30 17:27:20 +0530

    adding begging classifer and baagging using svm

---
+++ ensemble learning/bagging/baggingClassification.py
@@ -0,0 +1,41 @@
+from sklearn.datasets import make_classification
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+
+X,y = make_classification(n_samples=10000, n_features=10,n_informative=3)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+dt = DecisionTreeClassifier(random_state=42)
+dt.fit(X_train,y_train)
+y_pred = dt.predict(X_test)
+
+print("Decision Tree accuracy",accuracy_score(y_test,y_pred))
+
+bag = BaggingClassifier(
+    estimator=DecisionTreeClassifier(),
+    n_estimators=500,
+    max_samples=0.5,
+    bootstrap=True,
+    random_state=42
+)
+bag.fit(X_train,y_train)
+y_pred = bag.predict(X_test)
+print(accuracy_score(y_test,y_pred))
+print(bag.estimators_samples_[0].shape)
+print(bag.estimators_features_[0].shape)
+
+# BAGGING USING SVM
+bag = BaggingClassifier(
+    estimator=SVC(),
+    n_estimators=500,
+    max_samples=0.25,
+    bootstrap=True,
+    random_state=42
+)
+
+bag.fit(X_train,y_train)
+y_pred = bag.predict(X_test)
+print("Bagging using SVM",accuracy_score(y_test,y_pred))
